ee1dbcf0dbafe27d9f7d00e4593f9a3773080108
Masood Ahmadi <masoodahmadi553@gmail.com>
2023-12-04 15:50:01 +0200

    api tags added

--- pages/api/tags.js
+++ pages/api/tags.js
@@ -1,27 +1,5 @@
-// import axios from 'axios';
-
-// export default async function handler({ res, req }) {
-//   try {
-//     // const { city } = req.body;
-//     const API_key = 'ef92cd611ae451d1b7efb9cf50002de5';
-//     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Satakunta&APPID=${API_key}&units=metric`;
-
-//     const response = await axios.get(apiUrl);
-//     const newsData = response.data;
-
-//     return res.status(200).json(newsData);
-//   } catch (error) {
-//     console.error('Error fetching news:', error);
-//     return res.status(500).json({ message: 'Internal Server Error' });
-//   }
-// }
-// pages/api/searchNews.js
-import axios from 'axios';
-
 export default async function handler(req, res) {
   const formData = new FormData();
-  const accessToken = '';
-
   const newDoc = {
     url: 'https://app-yokot-we-tuni.azurewebsites.net/api/v1/tags',
     rooturl: '',
